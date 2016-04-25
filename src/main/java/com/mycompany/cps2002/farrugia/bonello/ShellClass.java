469b727a444dc996b055df5275d5a482479051c8
James Farrugia <james.j.farrugia.14@um.edu.mt>
2016-04-25 14:46:18 +0200

    First working commit

---
+++ src/main/java/com/mycompany/cps2002/farrugia/bonello/ShellClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cps2002.farrugia.bonello;
+
+/**
+ *
+ * @author James
+ */
+public class ShellClass {
+    
+    public int add(int a, int b){
+        return a + b;
+    }
+    
+}
---
+++ src/test/java/ShellTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author James
+ */
+
+import com.mycompany.cps2002.farrugia.bonello.ShellClass;
+import static junit.framework.Assert.assertTrue;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test
+ */
+public class ShellTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ShellTest() {
+        super();
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(ShellTest.class);
+    }
+
+    /**
+     * Rigorous test
+     */
+    public void testAdd(){
+        ShellClass test = new ShellClass();
+        assertEquals(5, test.add(3,2));
+        assertEquals(4, test.add(2,2)); 
+        assertEquals(0, test.add(0,0));
+        assertEquals(3, test.add(1,2));        
+    }
+}
