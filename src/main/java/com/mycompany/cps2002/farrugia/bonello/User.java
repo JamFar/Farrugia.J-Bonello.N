f06252a726fee8361b879c8ee6339c09434987d4
Nicky <nbonelloghio@hotmail.com>
2016-05-01 19:49:46 +0200

    Trying to find final missing testcase

--- src/main/java/com/mycompany/cps2002/farrugia/bonello/User.java
+++ src/main/java/com/mycompany/cps2002/farrugia/bonello/User.java
@@ -65,20 +65,15 @@ public class User {
                             this.currentlyLoanedBooks.add(book);
                             this.allLoanedBooks.add(book);
 
-                        } catch (OutOfBoundsException e) {
-                            System.err.println("Error in extracting current date.");
+                        } catch (OutOfBoundsException e) {System.err.println("Error in extracting current date.");
                         }
-                    } else {
-                        System.err.println("Book is already loaned out.");
+                    } else {System.err.println("Book is already loaned out.");
                     }
-                } else {
-                    System.err.println("User exceeds maximum loan allowance of 3 books.");
+                } else {System.err.println("User exceeds maximum loan allowance of 3 books.");
                 }
-            } else {
-                System.err.println("User is already currently loaning this book.");
+            } else {System.err.println("User is already currently loaning this book.");
             }
-        } else {
-            System.err.println("User has overdue books in possession.");
+        } else {System.err.println("User has overdue books in possession.");
         }
     }
 
