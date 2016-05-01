fe6683216e1e9a25080f5cf70353804b52577c92
Nicky <nbonelloghio@hotmail.com>
2016-05-01 19:53:38 +0200

    Previous result shows single line not being tested sufficiently. Added different number of lines to each else{} to discover which one is causing the error

--- src/main/java/com/mycompany/cps2002/farrugia/bonello/User.java
+++ src/main/java/com/mycompany/cps2002/farrugia/bonello/User.java
@@ -68,12 +68,22 @@ public class User {
                         } catch (OutOfBoundsException e) {System.err.println("Error in extracting current date.");
                         }
                     } else {System.err.println("Book is already loaned out.");
+                    System.err.println("Book is already loaned out.");
                     }
                 } else {System.err.println("User exceeds maximum loan allowance of 3 books.");
+                System.err.println("User exceeds maximum loan allowance of 3 books.");
+                System.err.println("User exceeds maximum loan allowance of 3 books.");
                 }
             } else {System.err.println("User is already currently loaning this book.");
+            System.err.println("User is already currently loaning this book.");
+            System.err.println("User is already currently loaning this book.");
+            System.err.println("User is already currently loaning this book.");
             }
         } else {System.err.println("User has overdue books in possession.");
+        System.err.println("User has overdue books in possession.");
+        System.err.println("User has overdue books in possession.");
+        System.err.println("User has overdue books in possession.");
+        System.err.println("User has overdue books in possession.");
         }
     }
 
