f55c9e436fafebf6d95901b97187347b6f00c520
Nicky <nbonelloghio@hotmail.com>
2016-06-01 20:13:51 +0200

    Removed redundant import

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,6 +1,5 @@
 
 import com.mycompany.cps2002.farrugia.bonello.Book;
-import com.mycompany.cps2002.farrugia.bonello.Catalogue;
 import com.mycompany.cps2002.farrugia.bonello.User;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
